c3c0f55f610645f0eca94439605764686acad886
Alexandre Felipe Moreira Iargan Buch <felipe.m.buch@gmail.com>
2023-05-19 10:15:47 -0300

    Configs

--- ios/pigzTests/pigzTests.m
+++ ios/pigzTests/pigzTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface ChamaIngressosTests : XCTestCase
+@interface pigzTests : XCTestCase
 
 @end
 
-@implementation ChamaIngressosTests
+@implementation pigzTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
